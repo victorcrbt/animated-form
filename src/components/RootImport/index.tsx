3788ce02b3fe17826e4dfddfadbcfc474e9e45fa
Victor Batalha <madivcb@gmail.com>
2020-03-19 14:01:42 -0300

    removed unused file

--- src/components/RootImport/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const RootImport: React.FC = () => {
-  return <Text>Hello, Root Import!</Text>;
-};
-
-export default RootImport;
