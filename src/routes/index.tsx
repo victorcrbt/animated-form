3c6a00767d8c8780b383c1b13ff74238f50adef6
Victor Batalha <madivcb@gmail.com>
2020-03-19 14:02:30 -0300

    route typings and add route for signin and singup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from '~/screens/Home';
+import { RootStackParamList } from './types';
 
-const Stack = createStackNavigator();
+import Home from '~/screens/Home';
+import SignIn from '~/screens/SignIn';
+import SignUp from '~/screens/SignUp';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 const Routes: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#fff' },
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
